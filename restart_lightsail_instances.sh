4901f92802cf05b9b39d40c1d3d96ab5909062dd
mahaonan1005 <mahaonan1993@gmail.com>
2023-03-28 06:09:13 +0800

    Add files via upload

---
+++ restart_lightsail_instances.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Stop instances
+aws lightsail get-instances | jq -r '.instances[] | .name' | xargs -I {} aws lightsail stop-instance --instance-name {}
+
+# Wait for 2 minutes
+sleep 2m
+
+# Start instances
+aws lightsail get-instances | jq -r '.instances[] | .name' | xargs -I {} aws lightsail start-instance --instance-name {}

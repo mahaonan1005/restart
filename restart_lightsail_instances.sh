5bd593f42d228fbe2f2be648f6de0884321c8122
mahaonan1005 <mahaonan1993@gmail.com>
2023-06-03 10:22:52 +0900

    Update restart_lightsail_instances.sh

--- restart_lightsail_instances.sh
+++ restart_lightsail_instances.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# 获取所有静态 IP 地址
+ips=$(aws lightsail get-static-ips --query 'staticIps[*].[name]' --output text)
+
+# 保存当前的 IFS 值，以便之后可以恢复
+OLDIFS=$IFS
+# 设置 IFS 为换行符
+IFS=$'\n'
+
+# 遍历每个 IP
+for ip_name in $ips
+do
+  # 首先解除实例关联
+  instance=$(aws lightsail get-static-ip --static-ip-name $ip_name --query 'staticIp.attachedTo' --output text)
+  
+  if [ "$instance" != "None" ]; then
+    echo "Detaching $ip_name from $instance..."
+    aws lightsail detach-static-ip --static-ip-name $ip_name
+  fi
+
+  # 如果 IP 存在，则删除
+  echo "Deleting $ip_name..."
+  aws lightsail release-static-ip --static-ip-name $ip_name
+done
+
+# 恢复原来的 IFS 值
+IFS=$OLDIFS
+
+sleep 5s
+
 # Stop instances
 aws lightsail get-instances | jq -r '.instances[] | .name' | xargs -I {} aws lightsail stop-instance --instance-name {}
 

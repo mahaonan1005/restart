971bbebbf6723a4ff6e2a05ff82d61fb83575999
mahaonan1005 <mahaonan1993@gmail.com>
2023-06-05 07:32:06 +0900

    Update restart_lightsail_instances.sh

--- restart_lightsail_instances.sh
+++ restart_lightsail_instances.sh
@@ -33,12 +33,12 @@ sleep 5s
 # Stop instances
 aws lightsail get-instances | jq -r '.instances[] | .name' | xargs -I {} aws lightsail stop-instance --instance-name {}
 
-# Wait for 30 seconds
-sleep 30s
+# Wait for 45 seconds
+sleep 45s
 
 # Start instances
 aws lightsail get-instances | jq -r '.instances[] | .name' | xargs -I {} aws lightsail start-instance --instance-name {}
 
-# Wait for 60 seconds
-sleep 60s
+# Wait for 45 seconds
+sleep 45s
 aws lightsail get-instances --query "instances[*].[name, publicIpAddress]" --output json | jq -r '.[] | @tsv' | sort
